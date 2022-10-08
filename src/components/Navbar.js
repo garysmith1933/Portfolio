a0f16589f8ece1fff00cfdb014104ae3b90b02c2
Gary Smith <garysmith193358@gmail.com>
2022-10-08 14:14:40 -0400

    created design of navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+export const Navbar = () => {
+    return (
+         <div className='navbar'> 
+            <div className='nav-logo'>GS</div>
+            <div className = 'nav-items'>   
+                <div className='nav-item navItem-underline'> 01 About </div>
+                <div className='nav-item navItem-underline'> 02 Work </div>
+                <div className='nav-item navItem-underline'> 03 Contact </div>
+                <div className='nav-item navItem-underline'> 04 Resume </div>
+            </div>
+        </div>       
+    )
+}
