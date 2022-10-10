1a051d5a68a7ced533ea6ebf3cd7b22186c80890
Gary Smith <garysmith193358@gmail.com>
2022-10-10 13:40:50 -0400

    added responsiveness to navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Navbar} from './components/Navbar.js'
 function App() {
   return (
     <div className="App">
-
       <div style={{display:'flex', justifyContent: 'center'}}>
         <Navbar/>
       </div>
