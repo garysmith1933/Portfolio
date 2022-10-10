6ccc1006223e784c698d55cb25a870d3e990deb8
Gary Smith <garysmith193358@gmail.com>
2022-10-10 18:05:01 -0400

    added ability for navbar to navigate to seperate sections on the page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import {Navbar} from './components/Navbar.js'
 import {Welcome} from './components/Welcome.js'
+import {About} from './components/About.js'
+import {Work} from './components/Work.js'
+import {Contact} from './components/Contact.js'
+import {useRef} from 'react'
 
 function App() {
+  const AboutRef = useRef(null)
+  const WorkRef = useRef(null)
+  const ContactRef = useRef(null)
+
+  const scrollToSection = (elementRef) => {
+    console.log('triggered')
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior:'smooth'
+    })
+  }
+ 
   return (
     <div className="App">
       <div style={{display:'flex', justifyContent: 'center'}}>
-        <Navbar/>
+        <Navbar scrollToSection={scrollToSection} AboutRef={AboutRef} WorkRef={WorkRef} ContactRef={ContactRef}/>
       </div>
 
-      <div style={{display:'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center', height: '50vh'}}>
+      <div style={{display:'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center', height: '100vh'}}>
         <Welcome/>
       </div>
+
+      <section ref={AboutRef} className='About'>
+        <About/>
+      </section>
+
+      <section ref={WorkRef} className='Work'>
+        <Work/>
+      </section>
+
+      <section ref={ContactRef} className='Contact'>
+        <Contact/>
+      </section>
+
 
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 
-export const Navbar = () => {
+export const Navbar = ({scrollToSection, AboutRef, WorkRef, ContactRef}) => {
 
     const toggleNav = () => {
         const nav = document.querySelector('.nav-items')
@@ -22,6 +22,22 @@ export const Navbar = () => {
         console.log(toggle.getAttribute('aria-expanded'))
     }
 
+    const toggleCheck = (elementRef) => {
+        const nav = document.querySelector('.nav-items')
+        const toggle = document.querySelector('.nav-toggle')
+        const isVisible = nav.getAttribute('data-visible')
+        const background = document.querySelector('#welcome-container')
+
+        //if the hamburger menu is open, we need to close it before we can navigate to the new section
+        if(isVisible === 'true') {
+            nav.setAttribute('data-visible', 'false')
+            toggle.setAttribute('aria-expanded', 'false')
+            background.classList.remove('blur')
+        }
+    
+        scrollToSection(elementRef)
+    }
+
     return (
          <div className='navbar'> 
             <div className='nav-logo'>GS</div>
@@ -32,10 +48,10 @@ export const Navbar = () => {
 
             <nav>
                 <ul id='nav-items' className='nav-items' data-visible='false'>   
-                    <li className='nav-item navItem-underline'> <span>01</span> About</li>
-                    <li className='nav-item navItem-underline'> <span>02</span> Work</li>
-                    <li className='nav-item navItem-underline'> <span>03</span> Contact</li>
-                    <li className='nav-item navItem-underline'> <span>04</span> Resume</li>
+                    <li className='nav-item navItem-underline' onClick={() => toggleCheck(AboutRef)}><span>01</span> About</li>
+                    <li className='nav-item navItem-underline' onClick={() => toggleCheck(WorkRef)} > <span>02</span> Work</li>
+                    <li className='nav-item navItem-underline' onClick={() => toggleCheck(ContactRef)}> <span>03</span> Contact</li>
+                    <li className='nav-item navItem-underline' > <span>04</span> Resume</li>
                 </ul>
             </nav>
             
