d661374a93d83c9b280cdd3663e09208024ea387
Gary Smith <garysmith193358@gmail.com>
2022-10-14 11:40:56 -0400

    added responsiveness to home page, fixed hamburger menu bug

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 export const Welcome = () => {
    return (
     <div className='greeting-container'>
-        <h4 className="greeting-name">Hi, my name is Gary!<span className="greeting-joke"> Yes, like the snail</span>.</h4>
-        <h1 className="greeting-detail">I can code or something like that.</h1>
-        <p className='greeting-story'> I'm a full stack web developer based in Ewing, NJ</p> 
+        <h4 className="greeting-intro">Hi, my name is <span className='greeting-name'>Gary!</span> Yes, like the snail.</h4>
+        <h1 className="greeting-detail">I enjoy making websites.</h1>
+        {/* <p className='greeting-story'> I'm a full stack web developer based in Ewing, NJ</p>  */}
         <div className='button'>See my Work</div>
     </div>
     
