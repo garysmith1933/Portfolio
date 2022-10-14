3dfcd9f2ea8e93e9f011dcbcbefde31aa9b5c5ce
Gary Smith <garysmith193358@gmail.com>
2022-10-14 14:22:09 -0400

    updated layout and design of about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 export const About = () => {
     return (
         <div className='about'>
-            <h1 className='about-title'>About Me</h1>
-
-            <div className='about-body'>
-                <h3 style={{textAlign: 'center'}}> My Story </h3>
+            <div className='about-header'>
+                <h1 className='about-title'>About</h1>
+                <div className='line right'></div>
+            </div>
+           
+            <div className='about-story'>
                  <p> 
                     I spent years prior to coding searching for the right outlet to express my creativity, from acting, voice acting, and even content creation once upon a time.
                     So when I started to learn how to code back in May 2021, I was shocked by how I addicted I was to it. It reminded me so much of playing video games in a way(which is my main hobby), being able to solve complex problems, and giving me a sense of a accomplishment as I learn and build new things. It was all I can ask for!  
-                    A handful of Udemy courses, Fullstack Academy, and some side projects later, here I am actively looking for a company to grow with as a fullstack web developer. 
+                    A handful of Udemy courses, Fullstack Academy, and some side projects later, here I am actively looking for a company to grow with as a full stack web developer. 
                 </p>
 
                 <p> When i'm not coding you can find me playing video games, cooking, or being forced to feed my two cats...by my two cats. </p>
