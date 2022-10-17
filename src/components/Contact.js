060f53648f4e5312a0b2626e578f4261a698aa46
Gary Smith <garysmith193358@gmail.com>
2022-10-17 15:36:31 -0400

    contact section is complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
 export const Contact = () => {
     return (
-        <div>
-            <h1>This is Contact Page</h1>
+        <div className='contact-container'>
+            <div className='section-header'>
+                <h1 className='about-title'>Contact</h1>
+                <div className='line'></div>
+            </div>
+
+            <div className='contact-content'>
+                <p className='contact-text'>I'm currently open to new opportunities! Think I would be a great fit for your team or just want to say hi? Feel free to shoot me a message. I'm always happy to chat!</p>
+                <div className='button cta'>
+                    <a href='mailto:garysmith193358@gmail.com'>Lets talk!</a>
+                </div>
+            </div>
         </div>
     )
 }
