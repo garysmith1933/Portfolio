0c749d6b8c85e51f130b9ba64023bb5ec9ba4693
Gary Smith <garysmith193358@gmail.com>
2023-06-19 15:56:18 -0400

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ export const About = () => {
                  <p className='story'> 
                  Prior to coding, I spent years searching for the right outlet to express my creativity. Acting, voice acting, even content creation on Twitch and Youtube once upon a time. 
                  When I started to learn how to code, I was shocked by how addicted I became. It reminded me of playing video games, which is my main hobby. I loved being able to solve complex problems and the sense of accomplishment I got as I learned and built new skills. 
-                 It was all I could ask for! I'm now currently looking for a company to grow with as a full stack web developer.
+                 It was all I could ask for! I'm now currently looking for a company to grow with as a software engineer.
                 </p>
 
                 <p className='story'> When i'm not coding you can find me playing video games, cooking, or being forced to feed my two cats...by my two cats. </p>
